2e12b8f6e45257e1b66e58fc0c90ab792735f20e
Your Name <youremail@example.com>
2023-08-23 16:41:35 +0300

    commit message

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,48 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
 def admin_login(username, password):
     # your code here
-    pass
+    if (username.lower() == "admin" and password == "12345"):
+        return "Access granted"
+    else:
+        return "Access denied"
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature < 40:
+        response = "brisk"
+    elif 40 <= temperature <= 65:
+        response = "a little chilly"
+    elif temperature > 85:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+    return f"It's {response} out there!"
+
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
+
+    
